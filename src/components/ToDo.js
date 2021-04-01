da857206133a11e8a2fd9d12cfce59abfb1800a4
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-01 23:26:53 +0200

    Render to-do name in ToDo component

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,11 @@
 import "./ToDo.css";
 
-export default function ToDo() {
+export default function ToDo({ name }) {
   return (
     <section className="to-do-component">
       <li className="to-do-list-item">
         <button className="remove-button">Remove</button>
-        <h3>Input here</h3>
+        <h3>{name}</h3>
         <button className="status-button">Status</button>
       </li>
     </section>

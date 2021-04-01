3f3f3f69e0b342a8040f83a103f7fd1ed5c85d44
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-01 22:47:53 +0200

    Add new to-do to array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import AddToDo from "./components/AddToDo.js";
 import ToDoList from "./components/ToDoList.js";
+import { useState } from "react";
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+
+  function handleAddToDos(taskName) {
+    const newTodos = [...toDos, { name: taskName, isDone: false }];
+    setToDos(newTodos);
+  }
+
   return (
     <div className="App">
       <header className="header">
-        <AddToDo />
+        <AddToDo onAddToDo={handleAddToDos} />
       </header>
       <main className="main">
         <ul>
--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,14 +1,22 @@
 import "./AddToDo.css";
-export default function AddToDo() {
+export default function AddToDo({ onAddToDo }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.targetM;
+    const formInput = form.toDo.value;
+    onAddToDo(formInput);
+    form.reset();
+  }
+
   return (
     <div className="add-to-do-wrapper">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Add a to do"
           className="to-do-bar"
-          name="to-do"
-          id="to-do"
+          name="toDo"
+          id="toDo"
           required
         />
         <button type="submit" className="add-button">

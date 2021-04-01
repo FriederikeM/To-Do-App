0b339ad570a42a26fd3335a1849c52664981530e
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-01 21:58:13 +0200

    Add form tag and input id

--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -2,16 +2,19 @@ import "./AddToDo.css";
 export default function AddToDo() {
   return (
     <div className="add-to-do-wrapper">
-      <input
-        type="text"
-        placeholder="Add a to do"
-        className="to-do-bar"
-        name="to-do"
-        required
-      />
-      <button type="submit" className="add-button">
-        Add
-      </button>
+      <form>
+        <input
+          type="text"
+          placeholder="Add a to do"
+          className="to-do-bar"
+          name="to-do"
+          id="to-do"
+          required
+        />
+        <button type="submit" className="add-button">
+          Add
+        </button>
+      </form>
     </div>
   );
 }

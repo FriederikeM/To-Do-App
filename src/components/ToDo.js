bd78b96bc995c4805fa9dbd61c65f042a0e4ca9e
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-02 12:43:21 +0200

    Switch color change to button instead of whole component

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -8,10 +8,13 @@ export default function ToDo({ name, onToggleStatus, isDone }) {
   const classForStatus = isDone ? "toDo--done" : "toDo--pending";
 
   return (
-    <li className={`to-do-list-item ${classForStatus}`}>
+    <li className="to-do-list-item">
       <button className="remove-button">Remove</button>
       <h3>{name}</h3>
-      <button className="status-button" onClick={handleToggleClick}>
+      <button
+        className={`status-button ${classForStatus}`}
+        onClick={handleToggleClick}
+      >
         {isDone ? "Done" : "Pending"}
       </button>
     </li>

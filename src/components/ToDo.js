c3e157f95605c24bff489df0078c82518d18ae74
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-02 12:39:40 +0200

    Add color change depending on status

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,8 +5,10 @@ export default function ToDo({ name, onToggleStatus, isDone }) {
     onToggleStatus(name);
   }
 
+  const classForStatus = isDone ? "toDo--done" : "toDo--pending";
+
   return (
-    <li className="to-do-list-item">
+    <li className={`to-do-list-item ${classForStatus}`}>
       <button className="remove-button">Remove</button>
       <h3>{name}</h3>
       <button className="status-button" onClick={handleToggleClick}>

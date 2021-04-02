211329ac869cee9855503adc0d9b3071c286e587
Friederike Moeser <friederike.moeser@gmail.com>
2021-04-02 21:46:29 +0200

    Add name change functionality

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,6 +1,7 @@
 import "./ToDo.css";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { AiFillCheckCircle } from "react-icons/ai";
+import { useState } from "react";
 
 export default function ToDo({ name, onToggleStatus, isDone, onDeleteToDo }) {
   function handleDeleteClick() {
@@ -14,10 +15,19 @@ export default function ToDo({ name, onToggleStatus, isDone, onDeleteToDo }) {
   const classForStatus = isDone ? "toDo--done" : "toDo--pending";
   const classForText = isDone ? "text-moved" : "text-unmoved";
 
+  const [taskName, setTaskName] = useState(`${name}`);
+
+  function handleNameClick() {
+    let newName = prompt("Please enter a new name");
+    if (newName !== "" && newName !== null) {
+      setTaskName(newName);
+    }
+  }
+
   return (
     <li className="to-do-list-item">
-      <h3 className={`task-name ${classForText}`}>
-        {name} {isDone ? <AiFillCheckCircle className="checkmark" /> : ""}
+      <h3 className={`task-name ${classForText}`} onClick={handleNameClick}>
+        {taskName} {isDone ? <AiFillCheckCircle className="checkmark" /> : ""}
       </h3>
       <div className="to-do-buttons">
         <button
